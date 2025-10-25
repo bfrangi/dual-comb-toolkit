01214035482a47beb3039c8e6ab6383c7defa274
Bernat Frangi <bernat.frangi@gmail.com>
2025-10-25 10:45:49 +0200

    Add `get_x_of_min_y` to `src/lib/math.py`

--- src/lib/math.py
+++ src/lib/math.py
@@ -1,4 +1,12 @@
-def bounded(value: int | float, min_value: int | float, max_value: int | float) -> int | float:
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from numpy import ndarray
+
+
+def bounded(
+    value: int | float, min_value: int | float, max_value: int | float
+) -> int | float:
     """
     Bound a value to be within a specified range.
 
@@ -17,3 +25,24 @@ def bounded(value: int | float, min_value: int | float, max_value: int | float) 
         The bounded value, which is guaranteed to be within the range [min_value, max_value].
     """
     return max(min(value, max_value), min_value)
+
+
+def get_x_of_min_y(x: "ndarray", y: "ndarray") -> float:
+    """
+    Get the x value at which the y is at its minimum.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The x data.
+    y : np.ndarray
+        The y data.
+
+    Returns
+    -------
+    float
+        The x value at which the y is at its minimum.
+    """
+    from numpy import argmin
+
+    return x[argmin(y)]
