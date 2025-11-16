c00c6fabd7f1bc614c0d5ddf36611183fdb349d8
Bernat Frangi <bernat.frangi@gmail.com>
2025-11-16 15:46:18 +0100

    Update line simulator script

--- src/line-simulator.py
+++ src/line-simulator.py
@@ -4,9 +4,10 @@ from lib.plots import use_latex
 # Define the molecule, VMR, pressure, temperature, and length.
 
 molecule = "CH4"
+database = "HITEMP"
 vmr = 0.01  # volume mixing ratio
 pressure = 101325  # Pa
-temperature = 298  # K
+temperature = 1200  # K
 length = 0.07  # m
 
 # Simulation configuration.
@@ -32,6 +33,7 @@ s = Simulator(
     length=length,
     use_gpu=use_gpu,
     wavelength_step=wl_step,
+    database=database,
 )
 s.compute_transmission_spectrum(wl_min=wl_min, wl_max=wl_max)
 s.show_transmission_spectrum()
